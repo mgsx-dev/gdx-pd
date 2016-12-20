04afa660ea717759383c9a0d3d477742dee4e55b
Germain AUBERT <germain.aubert@gmail.com>
2016-12-20 10:26:29 +0100

    gdx test projects generation

---
+++ tests/android/src/net/mgsx/pd/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.mgsx.pd;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.mgsx.pd.GdxPdTest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GdxPdTest(), config);
+	}
+}
---
+++ tests/desktop/src/net/mgsx/pd/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.mgsx.pd.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mgsx.pd.GdxPdTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxPdTest(), config);
+	}
+}
