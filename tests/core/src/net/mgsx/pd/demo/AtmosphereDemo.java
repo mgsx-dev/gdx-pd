4a1832e6216b828dfba7259aa00e0e3fceebf116
Germain AUBERT <germain.aubert@gmail.com>
2017-03-14 13:29:38 +0100

    improve demo
    add comments
    rework on sound effects
    add samples

--- tests/core/src/net/mgsx/pd/demo/AtmosphereDemo.java
+++ tests/core/src/net/mgsx/pd/demo/AtmosphereDemo.java
@@ -2,10 +2,12 @@ package net.mgsx.pd.demo;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
 
 import net.mgsx.pd.Pd;
 import net.mgsx.pd.patch.PdPatch;
@@ -18,6 +20,15 @@ public class AtmosphereDemo implements Demo
 	public Actor create(Skin skin) 
 	{
 		Table root = new Table(skin);
+		
+		String info = "Demonstrate procedural audio to generate atmosphere.\n" +
+				"Sound design is intentionally simple to help understanding.";
+		
+		Label infoLabel = new Label(info, skin);
+		infoLabel.setAlignment(Align.center);
+		
+		root.add(infoLabel).padBottom(30).colspan(4).row();
+
 		
 		addControl(root, "Rain", "rain");
 		addControl(root, "Wind", "wind");
--- tests/core/src/net/mgsx/pd/demo/SoundEffectsDemo.java
+++ tests/core/src/net/mgsx/pd/demo/SoundEffectsDemo.java
@@ -2,27 +2,36 @@ package net.mgsx.pd.demo;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
 
 import net.mgsx.pd.Pd;
 import net.mgsx.pd.patch.PdPatch;
 
 public class SoundEffectsDemo implements Demo
 {
-
-private PdPatch patch;
+	private PdPatch patch;
 	
 	@Override
 	public Actor create(Skin skin) 
 	{
 		Table root = new Table(skin);
 		
-		addControl(root, "Explosion", "boom");
-		addControl(root, "Shot", "shot");
-		addControl(root, "GUI", "gui");
+		String info = "Demonstrate both procedural and sample based sounds.\n" +
+				"Sound design is intentionally simple to help understanding.";
+		
+		Label infoLabel = new Label(info, skin);
+		infoLabel.setAlignment(Align.center);
+		
+		root.add(infoLabel).padBottom(30).colspan(4).row();
+		
+		addControl(root, "Explosion (procedural)", "explosion");
+		addControl(root, "GUI (procedural)", "gui");
+		addControl(root, "Buy (sample based)", "sample");
 		
 		patch = Pd.audio.open(Gdx.files.internal("pd/effects.pd"));
 		
@@ -31,24 +40,19 @@ private PdPatch patch;
 	
 	private void addControl(Table table, String label, final String receiver)
 	{
-		final TextButton low = new TextButton("low", table.getSkin());
-		low.addListener(new ChangeListener() {
-			@Override
-			public void changed(ChangeEvent event, Actor actor) {
-				Pd.audio.sendList(receiver, 0.5f);
-			}
-		});
-		final TextButton high = new TextButton("high", table.getSkin());
-		high.addListener(new ChangeListener() {
-			@Override
-			public void changed(ChangeEvent event, Actor actor) {
-				Pd.audio.sendList(receiver, 1f);
-			}
-		});
-		
 		table.add(label);
-		table.add(low);
-		table.add(high).row();
+		for(int i=0 ; i<3 ; i++){
+			final int value = i;
+			final TextButton bt = new TextButton("Variation " + String.valueOf(i+1), table.getSkin());
+			bt.addListener(new ChangeListener() {
+				@Override
+				public void changed(ChangeEvent event, Actor actor) {
+					Pd.audio.sendList(receiver, value);
+				}
+			});
+			table.add(bt);
+		}
+		table.row();
 	}
 
 	@Override
